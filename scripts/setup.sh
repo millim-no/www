dbbe2f56140dab986cff2d6c2335a4c05f9497c7
Michal Jan Warecki <mwarecki1@gmail.com>
2024-03-29 19:27:49 +0100

    Setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Bruk dette skriptet for fyrstegongsoppsett
+
+check_log="Støtte på ein feil, sjekk setup.log for info."
+log_path=`pwd`/setup.log
+
+echo "Aktiverer venv..."
+python3 -m venv ./venv && \
+  source venv/bin/activate && \
+  pip3 install -r requirements.txt > $log_path
+if [[ $? == 0 ]]
+then
+  rm $log_path
+  echo "Installerte requirements."
+else
+  echo $check_log
+  exit 1
+fi
+
+#if [[ ! -f "../../configs/travel-planner.json" ]]
+#then
+#  echo "Hugs å laste opp config-fila til ../configs/travel-planner.json "
+#  exit 1
+#else
+#  echo "Config-fil oppdaga."
+#fi
+
+echo "Lastar inn data frå databasen..."
+python3 manage.py migrate > $log_path &2> $log_path
+#python3 manage.py loaddata dummydata.json >> $log_path
+
+if [[ $? == 0 ]]
+then
+  rm $log_path
+  echo "Lasta inn data."
+else
+  echo $check_log
+  exit 1
+fi
